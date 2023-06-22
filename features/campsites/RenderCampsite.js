d8446663b302ad83caa16b392364130f745c831f
Kwanele <kwanele@Kwaneles-MacBook-Pro.local>
2023-06-22 07:28:28 -0600

    social-sharing

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, PanResponder, Alert } from "react-native"
+import { StyleSheet, Text, View, PanResponder, Alert, Share} from "react-native"
 import { Card, Icon } from "react-native-elements"
 import { baseUrl } from "../../shared/baseUrl"
 import * as Animatable from "react-native-animatable"
@@ -42,8 +42,23 @@ const RenderCampsite = props => {
     }
   })
 
+  const shareCampsite = (title, message, url) => {
+    Share.share(
+      {
+        title,
+        message: `${title}: ${message} ${url}`,
+        url
+      },
+      {
+        dialogTitle: "Share " + title
+      }
+    )
+  }
+
 
   if (campsite) {
+
+
     return (
       <Animatable.View animation="fadeInDownBig" duration={2000} delay={1000} {...panResponder.panHandlers} ref={view}>
         <Card containerStyle={styles.cardContainer}>
@@ -56,6 +71,7 @@ const RenderCampsite = props => {
           <View style={styles.cardRow}>
             <Icon name={props.isFavorite ? "heart" : "heart-o"} type="font-awesome" color="#f50" raised reverse onPress={() => (props.isFavorite ? console.log("Already set as a favorite") : props.markFavorite())} />
             <Icon name="pencil" type="font-awesome" color="#5637DD" raised reverse onPress={props.onShowModal} />
+            <Icon name="share" type="font-awesome" color="#5637DD" raised reverse onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} />
           </View>
         </Card>
       </Animatable.View>
